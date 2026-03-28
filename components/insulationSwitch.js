ae55dd0719dec97eb9bf1c8579688140483668fb
Ceo-Sammarco <noah.kd@gmail.com>
2026-03-28 16:31:08 -0300

    update:  M calc.html ?? components/insulationSwitch.js ?? helpers/helpers.js

---
+++ components/insulationSwitch.js
@@ -0,0 +1,10 @@
+/**
+ * --- insulationSwitch ---
+ *  */
+export const insulationSwitch = `
+  <switch-label>Incluir Lã de Vidro/Pet?</switch-label>
+  <label class="switch">
+    <input type="checkbox" id="i_insulation_toggle" />
+    <span class="slider"></span>
+  </label>
+`;
---
+++ helpers/helpers.js
@@ -0,0 +1,44 @@
+/**
+ * Seletor inteligente estilo jQuery
+ * @param {string} selector - O seletor CSS (ex: "#id", ".classe", "div")
+ * @param {Element} [context=document] - Onde procurar o elemento (opcional)
+ * @returns {Element|NodeList|null} - Retorna um elemento único, uma lista ou null
+ */
+export const $ = (selector, context = document) => {
+  const elements = context.querySelectorAll(selector);
+
+  // Se não encontrar nada, retorna null
+  if (elements.length === 0) return null;
+
+  // Se encontrar apenas um, retorna o elemento direto (estilo $('#id'))
+  // Se encontrar vários, retorna a NodeList (estilo $('.classe'))
+  return elements.length === 1 ? elements[0] : elements;
+};
+/* --- usage 
+const form = $('#serviceForm'); 
+Retorna o elemento direto
+
+const inputs = $('.input-width'); 
+Retorna uma NodeList (Array-like)
+inputs.forEach(i => console.log(i.value));
+
+Imagina que você tem o card de um serviço
+const card = $('#card-12345');
+const btnDelete = $('.btn-delete', card); 
+Procura o botão APENAS dentro desse card específico
+*/
+
+/**
+ * Seletor inteligente estilo jQuery
+ * @param {string} selector - O seletor CSS (ex: "#id", ".classe", "div")
+ * @param {Element} [context=document] - Onde procurar o elemento (opcional)
+ * @returns {Element|NodeList|null} - Retorna um elemento único, uma lista ou null
+ */
+export const $$ = (selector, context = document) => {
+  const elements = context.querySelectorAll(selector);
+
+  // Se não encontrar nada, retorna null
+  // if (elements.length === 0) return null;
+
+  return elements;
+};
